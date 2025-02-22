3f755e8c92775fd65665d11a7eec859c641132f0
AsakaLynux <asakalynux@gmail.com>
2025-02-22 23:13:26 +0700

    Add identifier screen

--- lib/view/favourite/widgets/favourite_screen.dart
+++ lib/view/favourite/widgets/favourite_screen.dart
@@ -5,6 +5,8 @@ class FavouriteScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Center(
+      child: Text("Favourite Screen"),
+    );
   }
 }
--- lib/view/wallet/widgets/wallet_screen.dart
+++ lib/view/wallet/widgets/wallet_screen.dart
@@ -5,6 +5,8 @@ class WalletScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Center(
+      child: Text("Wallet Screen"),
+    );
   }
 }
