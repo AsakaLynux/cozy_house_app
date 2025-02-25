c8f094240f0b10990d193b9b75ef5640fce95841
AsakaLynux <asakalynux@gmail.com>
2025-02-25 17:22:34 +0700

    Add red color

--- lib/view/core/themes/colors.dart
+++ lib/view/core/themes/colors.dart
@@ -6,3 +6,4 @@ Color blackColor = const Color(0xff000000);
 Color whiteColor = const Color(0xffffffff);
 Color greyColor = const Color(0xff82868E);
 Color cardBackgroundColor = const Color(0xffF6F7F8);
+Color redColor = const Color(0xffFB7072);
