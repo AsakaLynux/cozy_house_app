2f1ab9cc2f5f2985a7751090712653ad1dffb27d
AsakaLynux <asakalynux@gmail.com>
2025-02-25 14:53:15 +0700

    Delete bottom navigation bar comment

--- lib/view/main/widgets/main_screen.dart
+++ lib/view/main/widgets/main_screen.dart
@@ -134,7 +134,6 @@ class _MainScreenState extends State<MainScreen> with TickerProviderStateMixin {
 
     return Scaffold(
       backgroundColor: whiteColor,
-      // bottomNavigationBar: bottomNavigationBar(),
       body: Stack(
         children: [
           PageView(
