66925221dda3aa4cc23b347d1996e8e5863924f5
AsakaLynux <asakalynux@gmail.com>
2025-02-27 12:58:38 +0700

    Craete place model parameter

---
+++ lib/domain/models/place_model.dart
@@ -0,0 +1,29 @@
+class PlaceModel {
+  final int id;
+  final String name;
+  final String image;
+  final String address;
+  final String city;
+  final String country;
+  final int rating;
+  final int price;
+  final String createBy;
+  final DateTime createAt;
+  final String updateBy;
+  final DateTime updateAt;
+
+  PlaceModel({
+    required this.id,
+    required this.name,
+    required this.image,
+    required this.address,
+    required this.city,
+    required this.country,
+    required this.rating,
+    required this.price,
+    required this.createBy,
+    required this.createAt,
+    required this.updateBy,
+    required this.updateAt,
+  });
+}
