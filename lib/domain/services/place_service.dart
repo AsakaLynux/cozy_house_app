2ae89ad36e17e89e2920df398eb5f94854ffd8ab
AsakaLynux <asakalynux@gmail.com>
2025-02-27 14:03:09 +0700

    Add delete all and insert function in place service

--- lib/domain/services/place_service.dart
+++ lib/domain/services/place_service.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/foundation.dart';
 
+import '../../data/place_data.dart';
 import '../models/place_model.dart';
 import 'database_service.dart';
 
@@ -86,6 +87,54 @@ class PlaceService {
     } else {
       if (kDebugMode) {
         print("deletePlace(): Delete Place Failed");
+      }
+    }
+  }
+
+  void insertPlace() async {
+    final db = await databaseService.database;
+    int countData = 1;
+    for (PlaceModel place in listPlace) {
+      final insertData = await db.insert(
+        placeTableName,
+        {
+          placeNameColumnName: place.name,
+          placeImageColumnName: place.image,
+          placeAddressColumnName: place.address,
+          placeCityColumnName: place.city,
+          placeCountryColumnName: place.country,
+          placeRatingColumnName: place.rating,
+          placePriceColumnName: place.price,
+          placeCreateByColumnName: place.createBy,
+          placeCreateAtColumnName: place.createAt,
+          placeUpdateByColumnName: place.updateBy,
+          placeUpdateAtColumnName: place.updateAt,
+        },
+      );
+      if (insertData == 1) {
+        if (kDebugMode) {
+          print("inserPlaceData() $countData: Insert Place successed");
+        }
+      } else {
+        if (kDebugMode) {
+          print("inserPlaceData() $countData: Insert Place Failed");
+        }
+      }
+      countData++;
+    }
+  }
+
+  void deleteAllPlace() async {
+    final db = await databaseService.database;
+    final deleteData = await db.delete(placeTableName);
+
+    if (deleteData == 1) {
+      if (kDebugMode) {
+        print("deleteAllPlace(): Delete Place successed");
+      }
+    } else {
+      if (kDebugMode) {
+        print("deleteAllPlace(): Delete Place Failed");
       }
     }
   }
