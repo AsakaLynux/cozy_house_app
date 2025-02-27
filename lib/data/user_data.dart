7ada00ed8d2320b25997f2d814a468fb05322c2f
AsakaLynux <asakalynux@gmail.com>
2025-02-27 18:39:19 +0700

    Craete user data

---
+++ lib/data/user_data.dart
@@ -0,0 +1,34 @@
+import '../domain/models/user_model.dart';
+
+List<UserModel> listUser = [
+  UserModel(
+    id: 0,
+    userName: "irfan",
+    email: "irfan@email.com",
+    password: "123",
+    createBy: "admin",
+    createAt: DateTime.now(),
+    updateBy: "admin",
+    updateAt: DateTime.now(),
+  ),
+  UserModel(
+    id: 0,
+    userName: "asaka",
+    email: "asaka@email.com",
+    password: "321",
+    createBy: "admin",
+    createAt: DateTime.now(),
+    updateBy: "admin",
+    updateAt: DateTime.now(),
+  ),
+  UserModel(
+    id: 0,
+    userName: "admin",
+    email: "admin@email.com",
+    password: "123",
+    createBy: "admin",
+    createAt: DateTime.now(),
+    updateBy: "admin",
+    updateAt: DateTime.now(),
+  ),
+];
