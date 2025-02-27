bb25b59e0061ae57bd38437fe480d2c561a1dfad
AsakaLynux <asakalynux@gmail.com>
2025-02-27 13:40:27 +0700

    Add print to information on lo in user service

--- lib/domain/services/user_service.dart
+++ lib/domain/services/user_service.dart
@@ -10,13 +10,17 @@ class UserService {
     final db = await databaseService.database;
     final data = await db.query(userTableName);
     if (kDebugMode) {
-      print(data);
+      print("getUser(): $data");
     }
   }
 
-  void addUser(String userName, String email, String password) async {
+  void addUser(
+    String userName,
+    String email,
+    String password,
+  ) async {
     final db = await databaseService.database;
-    await db.insert(
+    final addData = await db.insert(
       userTableName,
       {
         userNameColumnName: userName,
@@ -28,10 +32,24 @@ class UserService {
         userUpdateAtColumnName: DateTime.now(),
       },
     );
+
+    if (addData == 1) {
+      if (kDebugMode) {
+        print("addUser(): Delete User successed");
+      }
+    } else {
+      if (kDebugMode) {
+        print("addUser(): Delete User Failed");
+      }
+    }
   }
 
   void updateUser(
-      int id, String userName, String email, String password) async {
+    int id,
+    String userName,
+    String email,
+    String password,
+  ) async {
     final db = await databaseService.database;
     final userData = await db.query(
       userTableName,
@@ -40,6 +58,7 @@ class UserService {
         id,
       ],
     );
+
     final user = userData
         .map(
           (e) => UserModel(
@@ -55,7 +74,8 @@ class UserService {
         )
         .toList()
         .first;
-    await db.update(
+
+    final updateData = await db.update(
       userTableName,
       {
         userNameColumnName: userName.isEmpty ? user.userName : userName,
@@ -71,16 +91,37 @@ class UserService {
         id,
       ],
     );
+    if (updateData == 1) {
+      if (kDebugMode) {
+        print("updateUser(): Delete User successed");
+        print("updateUser(): $userData");
+      }
+    } else {
+      if (kDebugMode) {
+        print("updateUser(): Delete User Failed");
+      }
+    }
   }
 
-  void deleteUser(int id) async {
+  void deleteUser(
+    int id,
+  ) async {
     final db = await databaseService.database;
-    await db.delete(
+    final deleteData = await db.delete(
       userTableName,
-      where: "id = ?",
+      where: "$userIdColumnName = ?",
       whereArgs: [
         id,
       ],
     );
+    if (deleteData == 1) {
+      if (kDebugMode) {
+        print("deleteUser(): Delete Place successed");
+      }
+    } else {
+      if (kDebugMode) {
+        print("deleteUser(): Delete Place Failed");
+      }
+    }
   }
 }
