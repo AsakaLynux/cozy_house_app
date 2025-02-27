6b938488a2723fe52a3d461ecc53ba72bb6b5f0d
AsakaLynux <asakalynux@gmail.com>
2025-02-27 14:02:20 +0700

    Create user and place data

---
+++ lib/data/place_data.dart
@@ -0,0 +1,18 @@
+import '../domain/models/place_model.dart';
+
+List<PlaceModel> listPlace = [
+  PlaceModel(
+    id: 0,
+    name: "",
+    image: "",
+    address: "",
+    city: "",
+    country: "",
+    rating: 0,
+    price: 0,
+    createBy: "admin",
+    createAt: DateTime.now(),
+    updateBy: "admin",
+    updateAt: DateTime.now(),
+  ),
+];
