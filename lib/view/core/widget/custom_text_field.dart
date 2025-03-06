2cbbe2106a664e04adef3dae88c774512718edaa
AsakaLynux <asakalynux@gmail.com>
2025-03-06 22:27:22 +0700

    Create custom text field

---
+++ lib/view/core/widget/custom_text_field.dart
@@ -0,0 +1,55 @@
+import 'package:flutter/material.dart';
+
+import '../themes/fonts.dart';
+
+class CustomTextField extends StatelessWidget {
+  final String title;
+  final TextEditingController controller;
+  final String? Function(String?)? validator;
+  final TextInputType keyboardType;
+  final bool obscure;
+  final String? hintText;
+  final String? errorText;
+  final Widget? suffixIcon;
+
+  const CustomTextField({
+    super.key,
+    required this.title,
+    required this.controller,
+    required this.keyboardType,
+    this.obscure = false,
+    this.validator,
+    this.hintText,
+    this.errorText,
+    this.suffixIcon,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      child: Column(
+        children: [
+          Text(
+            title,
+            style: blackTextStyle,
+          ),
+          TextFormField(
+            controller: controller,
+            validator: validator,
+            keyboardType: keyboardType,
+            obscureText: obscure,
+            decoration: InputDecoration(
+              hintText: hintText,
+              hintStyle: greyTextStyle,
+              errorText: errorText,
+              suffixIcon: suffixIcon,
+              border: OutlineInputBorder(
+                borderRadius: BorderRadius.circular(18),
+              ),
+            ),
+          )
+        ],
+      ),
+    );
+  }
+}
