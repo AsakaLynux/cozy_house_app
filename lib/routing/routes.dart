8a2312313ccd575cc6235734609f04a614b5a893
AsakaLynux <asakalynux@gmail.com>
2025-03-08 22:22:58 +0700

    Add login and register routes

--- lib/routing/routes.dart
+++ lib/routing/routes.dart
@@ -1,8 +1,9 @@
 abstract final class Routes {
   static const splash = "/";
   static const main = "/main";
+  static const login = "/login";
+  static const register = "/register";
   static const home = "home";
-  static String placeWithId(int id) => '$home/$id';
   static const houseDetail = ":id";
   static const favourite = "favourite";
   static const wallet = "wallet";
